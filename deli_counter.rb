633835628d53758e186def749b67e187a4b7f6d9
Tyler Landry <tyler.landry1996@outlook.com>
2019-04-18 08:21:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 def take_a_number(place, name)
   place.push(name)
   line = place.index(name)
-  puts "Welcome, #{name}. You are number #{place."
+  puts "Welcome, #{name}. You are number #{place.index"
 
 
 
